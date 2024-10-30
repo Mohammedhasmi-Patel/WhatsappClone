66ecb84c77eb174d00cf73e1c06f93ec3fd61eb2
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2024-10-30 23:52:44 +0530

    setup fronted

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import {
+  createBrowserRouter,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
